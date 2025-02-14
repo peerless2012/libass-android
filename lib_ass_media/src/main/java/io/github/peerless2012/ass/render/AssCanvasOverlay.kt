241ad7d8fa06ecfb7532fc1fad71a75f5f69effb
peerless2012 <peerless2012@126.com>
2025-02-14 19:48:48 +0800

    Update canvas render.

--- lib_ass_media/src/main/java/io/github/peerless2012/ass/render/AssCanvasOverlay.kt
+++ lib_ass_media/src/main/java/io/github/peerless2012/ass/render/AssCanvasOverlay.kt
@@ -10,13 +10,14 @@ import androidx.media3.common.util.Size
 import androidx.media3.common.util.UnstableApi
 import io.github.peerless2012.ass.kt.ASSRender
 
-
 @OptIn(UnstableApi::class)
 class AssCanvasOverlay(private val renderer: ASSRender) : CanvasOverlay(true) {
 
     private val paint = Paint().apply {
         xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_OVER)
     }
+
+    private var texDirty = true
 
     override fun configure(videoSize: Size) {
         super.configure(videoSize)
@@ -25,18 +26,28 @@ class AssCanvasOverlay(private val renderer: ASSRender) : CanvasOverlay(true) {
 
     override fun onDraw(canvas: Canvas, presentationTimeUs: Long) {
         val result = renderer.renderFrame(presentationTimeUs / 1000, true)
-        if (result == null || result.changed != 0) {
-            canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR)
+        if (result != null && result.changed == 0) {
+            return
         }
-        result?.images?.forEach { frame ->
-            val r = frame.color shr 24 and 0xFF
-            val g = frame.color shr 16 and 0xFF
-            val b = frame.color shr 8 and 0xFF
-            val a = 0xFF - frame.color and 0xFF
-            val color = (a shl 24) or (r shl 16) or (g shl 8) or b
+        if (result == null && !texDirty) {
+            return
+        }
 
-            paint.color = color
-            canvas.drawBitmap(frame.bitmap, frame.x.toFloat(), frame.y.toFloat(), paint)
+        canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR)
+        texDirty = false
+
+        result?.images?.let { frames ->
+            texDirty = true
+            frames.forEach { frame ->
+                val r = frame.color shr 24 and 0xFF
+                val g = frame.color shr 16 and 0xFF
+                val b = frame.color shr 8 and 0xFF
+                val a = 0xFF - frame.color and 0xFF
+                val color = (a shl 24) or (r shl 16) or (g shl 8) or b
+
+                paint.color = color
+                canvas.drawBitmap(frame.bitmap, frame.x.toFloat(), frame.y.toFloat(), paint)
+            }
         }
     }
 }
